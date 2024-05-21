cefd40a11e91ae6d102bf3adf5101c8e1b2c1d61
APrasad28 <ashutosh.prasad@realcoderz.in>
2024-05-21 14:58:01 +0530

    changes-2

--- src/config/config.js
+++ src/config/config.js
@@ -13,3 +13,27 @@ export const unavailableLandscape =
 // For Carousel
 export const noPicture =
   "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
+
+  // The standard Lorem Ipsum passage, used since the 1500s
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+// incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+// exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+// dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+// Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+// anim id est laborum.
+
+// The standard Lorem Ipsum passage, used since the 1500s
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+// incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+// exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+// dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+// Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+// anim id est laborum.
+
+// The standard Lorem Ipsum passage, used since the 1500s
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+// incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+// exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+// dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+// Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+// anim id est laborum.
--- src/index.js
+++ src/index.js
@@ -15,3 +15,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// The standard Lorem Ipsum passage, used since the 1500s
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+// incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+// exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+// dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+// Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+// anim id est laborum.
